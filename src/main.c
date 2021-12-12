ef87c1de81cf762e8974a9173f2f2d12733e2fcc
volokitty <v.volokitty@gmail.com>
2021-12-12 22:32:19 +0300

    feat: add test tokens

--- src/main.c
+++ src/main.c
@@ -5,5 +5,13 @@
 int
 main(int argc, char **argv)
 {
+    struct list *tokens = init_list();
+    
+    token_list_push(tokens, ID, "smile");
+    token_list_push(tokens, ID, "draw");
+    token_list_push(tokens, NUMBER, "0");
+    
+    print_token_list(tokens);
+
     return 0;
 }
