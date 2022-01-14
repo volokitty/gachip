56819beee41ea73d48e53b47c725045738c0d9be
volokitty <v.volokitty@gmail.com>
2022-01-14 18:25:40 +0300

    let's try to do it again

--- src/main.c
+++ src/main.c
@@ -1,18 +1,7 @@
-#include "include/token.h"
 #include "include/list.h"
-#include "include/token_list.h"
 
 int
 main(int argc, char **argv)
 {
-    struct list *tokens = init_list();
-    
-    token_list_push(tokens, ID, "smile");
-    token_list_push(tokens, ID, "draw");
-    token_list_push(tokens, NUMBER, "0");
-    
-    print_token_list(tokens);
-    free_token_list(tokens);
-
     return 0;
 }
