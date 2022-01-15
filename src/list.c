d1e42124e843d56df9302036d3ba15598eeb1597
volokitty <v.volokitty@gmail.com>
2022-01-15 17:01:32 +0300

    feat: replace spaces with tabs

--- src/list.c
+++ src/list.c
@@ -4,42 +4,42 @@
 struct list *
 init_list()
 {
-    struct list *l = malloc(sizeof(struct list));
+	struct list *l = malloc(sizeof(struct list));
     
-    if (!l) exit(1);
+	if (!l) exit(1);
     
-    return l;
+	return l;
 }
 
 void
 push(struct list *l, void *data)
 {
-    struct node *node = malloc(sizeof(node));
+	struct node *node = malloc(sizeof(node));
     
-    if (!node) exit(1);
+	if (!node) exit(1);
     
-    node->data = data;
+	node->data = data;
 
-    if (!l->head) {
-        l->head = node;
-        l->tail = node;
-    } else {
-        l->tail->next = node;
-        l->tail = node;
-    }
+	if (!l->head) {
+		l->head = node;
+		l->tail = node;
+	} else {
+		l->tail->next = node;
+		l->tail = node;
+	}
 }
 
 void
 free_list(struct list *l)
 {
-    if (!l || !l->head) exit(1);
+	if (!l || !l->head) exit(1);
+
+	struct node *node = l->head;
+	struct node *tmp;
     
-    struct node *node = l->head;
-    struct node *tmp;
-    
-    while (node) {
-        tmp = node->next;
-        free(node);
-        node = tmp;
-    }
+	while (node) {
+		tmp = node->next;
+		free(node);
+		node = tmp;
+	}
 }
